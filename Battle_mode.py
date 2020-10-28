116fb3202fdd208f9fd948aaa1d22623250ce8fd
Anthony Cintron Casiano <cintron_117399@students.pupr.edu>
2020-10-27 23:19:07 -0400

    Added the magic spell and run option to the input box, further precise positioning is required

--- Battle_mode.py
+++ Battle_mode.py
@@ -47,27 +47,29 @@ def battle(win,win2,win4,win5,hero):
                     message36.draw(win5)
                     win2.getMouse()
                     win5.close()
-                    break
+                    
             else:
                 message37 = Text(Point(260,440),f"You get an urge to sneeze, the enemy {enemy.getname()} takes this chance to attack you.")  
                 message37.draw(win5)  
                 hero.health = hero.health - enemy.strength
                 message38 = Text(Point(260,460),f"Because of this, you have {hero.health} health remaining.")
                 message38.draw(win5)
-                win2.getMouse()
-                win5.close()
+                
 
 
 
         elif choice == "2":
-            print("(Casting spell)..Perish", enemy.getname(),",! The flames of this spell will render you useless.")
+            message39 = Text(Point(240,50),f"(Casting spell)..Perish {enemy.getname()}! The flames of this spell will render you useless.")
+            message39.draw(win5)
             hitchance = random.randint(0,10)
             if hitchance > 3:
                 enemy.health = enemy.health - hero.strength
-                print("You hurt the", enemy.getname(), "Their health is",enemy.health)
+                message40 = Text(Point(240,70),f"You hurt the {enemy.getname()} Their health is {enemy.health}")
+                message40.draw(win5)
                 if enemy.health > 0:
                     hero.health = hero.health - (enemy.strength / hero.defense)
-                    print ("The", enemy.getname(), "attack you. You have", hero.health,"health remaining.")
+                    message41 = Text(Point(240,90),f"The {enemy.getname()}, attack you. You have {hero.health}, health remaining.")
+                    message41.draw(win5)
                 else:
                     if enemy.getname() == "goblin":
                         enemy.health = 30
@@ -75,22 +77,30 @@ def battle(win,win2,win4,win5,hero):
                         enemy.health = 50
                     elif enemy.getname() == "vampire":
                         enemy.health = 60
-                    print("You have defeated the", enemy.getname())
+                    message42 = Text(Point(240,120),f"You have defeated the {enemy.getname()}")
+                    message42.draw(win2)
                     break
             else:
-                print("For some reason you forgot how to cast your spell, the enemy", enemy.getname(),"takes this chance to attack you.")    
+                message43 = Text(Point(240,140),f"For some reason you forgot how to cast your spell,\n the enemy {enemy.getname()} takes this chance to attack you.") 
+                message43.draw(win5)   
                 hero.health = hero.health - enemy.strength
-                print("Because of this, you have", hero.health, "health remaining.")
+                message44 = Text(Point(240,160),f"Because of this, you have {hero.health} health remaining.")
+                message44.draw(win5)
 
         elif choice == "3":
-            print("I must flee...", enemy.getname(),"'s power is overwhelming")
+            message45 = Text(Point(240,60),f"I must flee... {enemy.getname()}'s power is overwhelming")
+            message45.draw(win5)
             flee = random.randint(1,10)
             if flee > 4:
-                print ("You got away")
+                message46 = Text(Point(240,80),f"You got away")
+                message46.draw(win5)
                 break
             else:
-                print("You tried to flee but smacked the wall behind you. The", enemy.getname(), "attacks.")
+                message46 = Text(Point(240,90),f"You tried to flee but smacked the wall behind you. The {enemy.getname()} attacks.")
+                message46.draw(win5)
                 hero.health = hero.health -  enemy.strength
-                print("Your health is now",hero.health)
+                message47 = Text(Point(240,110),f"Your health is now {hero.health}")
+                message47.draw(win5)
         else:
-            print("Number not allowed, must be between 1, 2, or 3")
+            message48 = Text(Point(240,110),"Number not allowed, must be between 1, 2, or 3")
+            message48.draw(win5)
